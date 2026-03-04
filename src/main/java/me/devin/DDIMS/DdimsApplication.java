4be6ebcf64639eaf3133586c462fe4a03707a817
Devin-LM <devinlmason@gmail.com>
2026-03-04 17:17:19 -0500

    edited pom.xml to include required libraries

---
+++ src/main/java/me/devin/DDIMS/DdimsApplication.java
@@ -0,0 +1,14 @@
+package me.devin.DDIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdimsApplication.class, args);
+    }
+
+}
+
